ff58c60af7186cc631492ff581a35097cf1bece6
kezaris333 <mina.mina.01102005@gmail.com>
2025-09-25 17:06:09 +0300

    Update all.sql

--- 1lab/all.sql
+++ 1lab/all.sql
@@ -2,51 +2,53 @@
  --CREATE DATABASE t01_library;
  
  create type state_type as enum ('excellent', 'good', 'satisfactory', 'dilapidated', 'lost');
- create type where_type as enum ('in stock', 'issued', 'reversed');
+ create type where_type as enum ('in stock', 'issued', 'reserved');
  
 
- 
  create table if not exists public.author (
- author_key serial primary key,
+ author_id serial primary key,
  surname varchar (30) not null, 
  name varchar (30) not null
  );
  
  
+  create table if not exists public.publishing_house ( 
+ publishing_house_key serial primary key, 
+ name varchar(100) not null,
+ city varchar (50) not null
+ );
+ 
+  
+   create table if not exists public.book (
+book_id serial primary key, 
+title varchar(100) not null,
+author_id INT not null, 
+FOREIGN KEY (author_id) REFERENCES public.author(author_id),
+publishing_house_id INT not null,
+foreign key (publishing_house_id) references public.publishing_house (publishing_house_key),
+version VARCHAR(50) NOT NULL, --
+publication_year INT NOT NULL, --
+circulation INT NOT NULL --
+
+ );
+ 
+   
   create table if not exists public.reader (
- library_card serial primary key,
+ reader_id serial primary key,
  surname varchar (30) not null, 
  name varchar (30) not null,
- date_born date not null,
+ birth_date date not null,
  gender char (1) check (gender in ('m','f')) not null,
  registration_date date not null
  );
  
- create table if not exists public.publishing_house ( 
- house_key serial primary key, 
- title varchar(100) not null,
- city varchar (50) not null
- );
- 
- create table if not exists public.book (
-book_key serial primary key, 
-title varchar(100) not null,
-author_key INT not null, 
-FOREIGN KEY (author_key) REFERENCES public.author(author_key),
-city varchar (50) not null,
-code_pub_h INT not null,
-version VARCHAR(50) NOT NULL,
-year INT NOT NULL,
-circulation INT NOT NULL,
-foreign key (code_pub_h) references public.publishing_house (house_key)
- );
- 
+
 
  
  create table if not exists public.book_instance (
- internal_inventory_number serial primary key,
- book_information int not null,
- foreign key (book_information) references public.book(book_key),
+ instance_id serial primary key,
+ book_id int not null,
+ foreign key (book_id) references public.book(book_id),
  state state_type not null, 
  status where_type, --если утеряна
 location varchar (300) CHECK (location ~ '^/\d+/\d+/\d+$') 
@@ -56,31 +58,32 @@ location varchar (300) CHECK (location ~ '^/\d+/\d+/\d+$')
  
  
  create table if not exists public.issuance (
- date_issuance timestamp not NULL,
- expected_date_return date not NULL,
- actual_date_return date NULL,
- reader_ticket_id INT NOT NULL,
- book_instance_id INT NOT NULL,
-  FOREIGN KEY (reader_ticket_id) REFERENCES public.reader(library_card),
-  FOREIGN KEY (book_instance_id) REFERENCES public.book_instance(internal_inventory_number),
-  primary key (reader_ticket_id, book_instance_id)
+ issue_date timestamp not NULL,
+ expected_return_date date not NULL,
+ actual_return_date date NULL,
+ reader_id INT NOT NULL,
+ instance_id int not null,
+  FOREIGN KEY (reader_id) REFERENCES public.reader(reader_id),
+  FOREIGN KEY (instance_id) REFERENCES public.book_instance(instance_id),
+  primary key (reader_id, instance_id)
  );
  
  
  create table if not exists public.booking (
- number_reservation serial primary key,
- id_reader_number int,
- foreign key (id_reader_number) references public.reader (library_card),
- information_of_book int,
- foreign key (information_of_book) references public.book (book_key),
+ booking_id serial primary key,
+ reader_id int,
+ foreign key (reader_id) references public.reader (reader_id),
+ book_id int,
+ foreign key (book_id) references public.book (book_id),
  min_condition state_type not null,
  date_reservation date not null,
- time_reservation time not NULL
+ time_reservation timestamp(0) not null -- (0) точность, у нас до секунд
  );
  
  
  
- 
+  
+  
  
  
  
